c0fc1513b7f7601e1612aec0b233850b844e6221
Nandhana Selvam <nandhanaselvam@gmail.com>
2024-07-23 19:16:10 -0700

    Basic folder/file structure set up

---
+++ app/(pages)/day1/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "../_styles/day1/day1.module.scss";
+
+export default function Day1() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        DAY 1 ASSIGNMENT 
+      </div>
+    </main>
+  );
+}
