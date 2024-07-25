26ce59fc5aa1c653461f84fb79029ba77d525c01
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-07-24 17:02:21 -0700

    position image with transform

--- app/(pages)/day1/page.jsx
+++ app/(pages)/day1/page.jsx
@@ -1,11 +1,23 @@
 import styles from "./page.module.scss";
+import Image from "next/image";
 
 export default function Day1() {
   return (
     <main className={styles.main}>
-      <div className={styles.body}>
-        hello world :D
+      <div className={styles.topSection}>
+        <div className={styles.topContent}>
+            <h2>Our mission is to design creative digital solutions that have a positive social impact</h2>
+            <div className={styles.designImageContainerContainer}>
+              <div className={styles.designImageContainer}>
+                <Image src="/design-home.png" alt="figma design" fill="true" className={styles.designImage}></Image>
+              </div>
+            </div>
+        </div>
       </div>
+      <div className={styles.bottomSection}>
+        bottom
+      </div>
+      
     </main>
   );
 }
