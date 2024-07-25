2927470eb32d1fd40ebedb359ce06b8876694e4d
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-07-24 17:22:27 -0700

    position image relative to bottom section

--- app/(pages)/day1/page.jsx
+++ app/(pages)/day1/page.jsx
@@ -7,15 +7,20 @@ export default function Day1() {
       <div className={styles.topSection}>
         <div className={styles.topContent}>
             <h2>Our mission is to design creative digital solutions that have a positive social impact</h2>
-            <div className={styles.designImageContainerContainer}>
-              <div className={styles.designImageContainer}>
-                <Image src="/design-home.png" alt="figma design" fill="true" className={styles.designImage}></Image>
-              </div>
-            </div>
         </div>
+          
       </div>
       <div className={styles.bottomSection}>
-        bottom
+        <div className={styles.designImageContainer}>
+          <Image src="/design-home.png" alt="figma design" fill="true" className={styles.designImage}></Image>
+        </div>
+        <div className={styles.recentWork}>
+          <div className={styles.recentWorkTitleIcon}>
+            <Image src="" alt="pen icon" height="80" width="80"></Image>
+            <h2>Recent Work</h2>
+          </div>
+          <p>Transforming ideas into incredible realities is what we do best.</p>
+        </div>
       </div>
       
     </main>
