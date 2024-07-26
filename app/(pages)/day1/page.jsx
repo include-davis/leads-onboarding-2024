18fbf02ef3750f62ba780fdaa8c87274ce90dcf6
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2024-07-26 14:49:30 -0700

    Styled Recent-Work header

--- app/(pages)/day1/page.jsx
+++ app/(pages)/day1/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import { FaPenNib } from "react-icons/fa";
 
 export default function Page() {
   return (
@@ -11,7 +12,11 @@ export default function Page() {
             <img src="/design-home.png" className={styles.img_design_home}
               alt="Screenshots of the dashboard project showing desktop version"/>
           </div>
-          <div>text and icon</div>
+          <div className={styles.header_recent_work}>
+            <FaPenNib style={{fontSize: "2rem", color: "white"}}></FaPenNib>
+            <h5 style={{marginBottom: "30px", marginTop: "20px"}}>Recent Work</h5>
+            <h6>Transforming ideas into incredible realities is what we do best.</h6>
+          </div>
           <div>cards</div>
         </div>
       </div>
