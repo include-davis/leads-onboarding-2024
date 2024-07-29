ecec2fd2a85fcf1cd4c4833a002ae21ce3c85603
michelleyeoh <michellew.yeoh@gmail.com>
2024-07-29 16:26:00 -0700

    day2

--- app/(pages)/day_one/include.jsx
+++ app/(pages)/day_one/include.jsx
@@ -5,43 +5,44 @@ const images = {
     pen: '/images/pen.png',
     figma: '/images/header1.png'
 }
-const ielc = {
-    cohort: "WINTER/SPRING 2023",
-    img: "/images/IELC Tutoring.png",
-    img_alt: "IELC Tutoring",
-    title: "IELC Tutoring",
-    desc: "Website and brand identity project for the Interactive Elementary Learning Center to boost credibility and increase participation in their free 7-week summer program.",
-    href: "https://www.ielctutoring.com/"
-}
 
-const bandv = {
-    cohort: "FALL/WINTER 2023",
-    img: "/images/Bloom and Vine.png",
-    img_alt: "Bloom and Vine",
-    title: "Bloom and Vine",
-    desc: "Website project for Bloom and Vine a boutique floral design studio located in Northern California. Bloom and Vine strives to make every event unforgettable.",
-    href: "https://www.bloomandvine.net"
-}
-
-const nvsi = {
-    cohort: "WINTER/SPRING 2022",
-    img: "/images/New Vietnam Studies Initiative.png",
-    img_alt: "New Vietnam Studies Initiative",
-    title: "New Vietnam Studies Initiative",
-    desc: "We developed the website for NVSI, promoting research and collaboration in fields related to contemporary Vietnam's economic, political, and cultural development.",
-    href: "https://newvietnam.ucdavis.edu/"
-}
-
-const oneloop = {
-    cohort: "FALL/WINTER 2021",
-    img: "/images/ONELOOP.png",
-    img_alt: "ONELOOP",
-    title: "ONELOOP",
-    desc: "Oneloop is a team of graduate and undergraduate students at UCD. They design and build a Hyperloop pod to compete in SpaceX's annual Hyperloop competition.",
-    href: "https://davishyperloop.com/"
+const card_content = {
+    ielc: {
+        cohort: "WINTER/SPRING 2023",
+        img: "/images/IELC Tutoring.png",
+        img_alt: "IELC Tutoring",
+        title: "IELC Tutoring",
+        desc: "Website and brand identity project for the Interactive Elementary Learning Center to boost credibility and increase participation in their free 7-week summer program.",
+        href: "https://www.ielctutoring.com/"
+    },
+    bandv: {
+        cohort: "FALL/WINTER 2023",
+        img: "/images/Bloom and Vine.png",
+        img_alt: "Bloom and Vine",
+        title: "Bloom and Vine",
+        desc: "Website project for Bloom and Vine a boutique floral design studio located in Northern California. Bloom and Vine strives to make every event unforgettable.",
+        href: "https://www.bloomandvine.net"
+    },
+    nvsi: {
+        cohort: "WINTER/SPRING 2022",
+        img: "/images/New Vietnam Studies Initiative.png",
+        img_alt: "New Vietnam Studies Initiative",
+        title: "New Vietnam Studies Initiative",
+        desc: "We developed the website for NVSI, promoting research and collaboration in fields related to contemporary Vietnam's economic, political, and cultural development.",
+        href: "https://newvietnam.ucdavis.edu/"
+    },
+    oneloop: {
+        cohort: "FALL/WINTER 2021",
+        img: "/images/ONELOOP.png",
+        img_alt: "ONELOOP",
+        title: "ONELOOP",
+        desc: "Oneloop is a team of graduate and undergraduate students at UCD. They design and build a Hyperloop pod to compete in SpaceX's annual Hyperloop competition.",
+        href: "https://davishyperloop.com/"
+    }
 }
 
 export function Include() {
+    const positions = Object.values(card_content);
     return(
         <div className={styles.page}>
             <div className={styles.header1}>
@@ -56,10 +57,17 @@ export function Include() {
                 <h3>Transforming ideas into incredible realities is what we do best.</h3>
             </div>
             <div className={styles.card}>
-                <ProjectCard cohort={ielc.cohort} image={ielc.img} image_alt={ielc.img_alt} title={ielc.title} description={ielc.desc} url={ielc.href}></ProjectCard>
-                <ProjectCard cohort={bandv.cohort} image={bandv.img} image_alt={bandv.img_alt} title={bandv.title} description={bandv.desc} url={bandv.href}></ProjectCard>
-                <ProjectCard cohort={nvsi.cohort} image={nvsi.img} image_alt={nvsi.img_alt} title={nvsi.title} description={nvsi.desc} url={nvsi.href}></ProjectCard>
-                <ProjectCard cohort={oneloop.cohort} image={oneloop.img} image_alt={oneloop.img_alt} title={oneloop.title} description={oneloop.desc} url={oneloop.href}></ProjectCard>
+                {positions.map((obj, index) => (
+                    <ProjectCard
+                        cohort={obj.cohort}
+                        image={obj.img}
+                        image_alt={obj.img_alt}
+                        title={obj.title}
+                        description={obj.desc}
+                        url={obj.href}
+                        key={index}
+                    />
+                ))}
             </div>
             
 
--- app/(pages)/page.jsx
+++ app/(pages)/page.jsx
@@ -7,6 +7,7 @@ export default function Home() {
       <div className={styles.description}>
         <nav>
           <a href="/day_one">Day 1</a>
+          <a href="/day_two">Day 2</a>
         </nav>
       </div>
     </main>
