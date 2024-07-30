dd1058d30f811c35a99f96a3a49b5f2b4ec5400e
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-07-30 13:35:06 -0700

    .

--- app/(pages)/day1/page.jsx
+++ app/(pages)/day1/page.jsx
@@ -1,7 +1,11 @@
-import './page.scss';
+import styles from '@/(pages)/_styles/days/day1.module.scss';
 
-export default function Hello() {
-    return (
-        <div>hello</div>
+// import styles from '../_styles/days/day1.module.scss';
+
+export default function Day1(){
+    return(
+        <div>
+            please work
+        </div>
     )
 }
