cc2570bba562a4d09366cd26de52b9c80617d336
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-07-30 14:00:37 -0700

    Adding css

--- app/(pages)/day1/page.jsx
+++ app/(pages)/day1/page.jsx
@@ -1,11 +1,22 @@
-import Styles from '@/(pages)/_styles/days/day1.module.scss';
+// import Styles from '@/(pages)/_styles/days/day1.module.scss';
+// import styles from "../_styles/days/page.module.scss";
+
+import styles from '../_styles/days/day1.module.scss';
 
 // import styles from '../_styles/days/day1.module.scss';
 
 export default function Day1(){
     return(
-        <div>
-            please work
+        <div className={styles.pagewrapper}>
+          <div className={styles.workcontainer}>
+              <img src="pen.png"/>
+              <h1>Recent Work</h1>
+              <p>Transforming ideas into incredible realities is what we do best.</p>
+
+              <div className={styles.cardcontainer}>
+
+              </div>
+          </div>
         </div>
     )
 }
