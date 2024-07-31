1f0fb02403a26c70d29614ef97a59b5766048971
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-07-30 17:53:36 -0700

    Added images to cards

--- app/(pages)/_components/day1/project_cards.jsx
+++ app/(pages)/_components/day1/project_cards.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import styles from '../../_styles/days/day1.module.scss';
 
-export default function Card({quarter, image, projectName, description, link}) {
+export default function Card({cohort, image, title, description, url}) {
     return (
         <div className={styles.card}>
-            <p className={styles.quarter}>{quarter}</p>
-            <img className={styles.card_img}/>
+            <p className={styles.quarter}>{cohort}</p>
+            <img src={image} className={styles.card_img}/>
             <hr/>
-            <h4 className={styles.card_name}>{projectName}</h4>
+            <h4 className={styles.card_name}>{title}</h4>
             <p className={styles.card_description}>{description}</p>
-            <a target="_blank" ahref={link}>
+            <a target="_blank" ahref={url}>
                 <button className={styles.card_button}>
                     <span className={styles.card_dot}>●</span>
                     VISIT SITE
--- app/(pages)/day1/page.jsx
+++ app/(pages)/day1/page.jsx
@@ -4,7 +4,7 @@ import Card from '../_components/day1/project_cards';
 const card_content = {
   ielc: {
       cohort: "WINTER/SPRING 2023",
-      img: "/images/IELC Tutoring.png",
+      img: "/tutor.png",
       img_alt: "IELC Tutoring",
       title: "IELC Tutoring",
       desc: "Website and brand identity project for the Interactive Elementary Learning Center to boost credibility and increase participation in their free 7-week summer program.",
@@ -12,7 +12,7 @@ const card_content = {
   },
   bandv: {
       cohort: "FALL/WINTER 2023",
-      img: "/images/Bloom and Vine.png",
+      img: "/bloom.png",
       img_alt: "Bloom and Vine",
       title: "Bloom and Vine",
       desc: "Website project for Bloom and Vine a boutique floral design studio located in Northern California. Bloom and Vine strives to make every event unforgettable.",
@@ -20,7 +20,7 @@ const card_content = {
   },
   nvsi: {
       cohort: "WINTER/SPRING 2022",
-      img: "public/New Vietnam Studies Initiative.png",
+      img: "/viet.png",
       img_alt: "New Vietnam Studies Initiative",
       title: "New Vietnam Studies Initiative",
       desc: "We developed the website for NVSI, promoting research and collaboration in fields related to contemporary Vietnam's economic, political, and cultural development.",
@@ -28,7 +28,7 @@ const card_content = {
   },
   oneloop: {
       cohort: "FALL/WINTER 2021",
-      img: "ONELOOP.png",
+      img: "/one.png",
       img_alt: "ONELOOP",
       title: "ONELOOP",
       desc: "Oneloop is a team of graduate and undergraduate students at UCD. They design and build a Hyperloop pod to compete in SpaceX's annual Hyperloop competition.",
