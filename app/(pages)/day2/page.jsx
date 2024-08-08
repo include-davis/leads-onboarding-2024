760c5447ead8ec8002cc15303d2e7aa3743fbc20
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-08-08 10:44:01 -0700

    Created day2

---
+++ app/(pages)/day2/page.jsx
@@ -0,0 +1,9 @@
+import styles from '../_styles/days/day2.module.scss';
+
+export default function Day2(){
+    return(
+        <div className={styles.container}>
+            hiiii
+        </div>
+        )
+    }
