74b937a1639f7b30ca3e0cd21a9af11e050f5416
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-08-08 10:57:57 -0700

    Titling

--- app/(pages)/day2/page.jsx
+++ app/(pages)/day2/page.jsx
@@ -3,7 +3,7 @@ import styles from '../_styles/days/day2.module.scss';
 export default function Day2(){
     return(
         <div className={styles.container}>
-            hiiii
+            <h1>Carousel</h1>
         </div>
         )
     }
