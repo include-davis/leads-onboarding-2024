b009cbb6fb742610e7c9d4b21b6fd6adda6d8749
Nandhana Selvam <nandhanaselvam@gmail.com>
2024-08-10 16:46:36 -0700

    light/dark mode functionality completed

--- app/(pages)/day2/page.jsx
+++ app/(pages)/day2/page.jsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 import styles from "../_styles/day2/page.module.scss";
 import { useState, useEffect, useRef, useContext} from 'react';
 import { RxArrowLeft, RxArrowRight } from 'react-icons/rx'
+import { FaSun , FaMoon } from "react-icons/fa";
 
 export default function Day2() {
-    const data = ['Frame 1', 'Frame 2', 'Frame 3', 'Frame 4'];
+    const data = [
+      'Frame 1', 'Frame 2', 'Frame 3', 'Frame 4'
+    ];
     const [activeIndex, setActiveIndex] = useState(0)
     const [darkMode, setDarkMode] = useState(false); 
 
@@ -20,14 +23,37 @@ export default function Day2() {
     const changeTheme = () => {
       setDarkMode(!darkMode);
       document.body.classList.toggle('dark-mode', !darkMode);
-    };
+    }
+
+    const makeDark = () => {
+      if (!darkMode) {
+        setDarkMode(true);
+        document.body.classList.add('dark-mode', !darkMode);
+      }
+    }
+
+    const makeLight = () => {
+      if(darkMode) {
+        setDarkMode(false);
+        document.body.classList.remove('dark-mode', !darkMode);
+      }
+    }
 
     return (
       <div className={styles.main_container}>
+        <div className = {styles.buttons}>
+          <button 
+            className={`${styles.light_mode_button} ${!darkMode ? styles.active : ''}`} 
+            onClick={makeLight}>
+            <FaSun />
+          </button>
+          <button 
+            className={`${styles.dark_mode_button} ${darkMode ? styles.active : ''}`}
+            onClick={makeDark}>
+            <FaMoon />
+          </button>
+        </div>
         <h1 className={styles.title}> Carousel </h1>
-        <button className={styles.toggle_button} onClick={changeTheme}>
-                {darkMode ? 'Light Mode' : 'Dark Mode'}
-        </button>
         <div className={styles.window_container}>
           <button className={styles.arrow} onClick={subtractIndex}>
             <RxArrowLeft/>
@@ -38,18 +64,16 @@ export default function Day2() {
               style={{transform: `translateX(calc(${-activeIndex} * 100%))`}}
               >
               {data.map((frame, index) => {
-                            return (
-                                <div 
-                                    key={index} 
-                                    className={styles.frame} 
-                                    style={{ transform: (Math.abs(index - activeIndex) === 1 ||
-                                      (index === 0 && activeIndex === data.length - 1) ||
-                                      (index === data.length - 1 && activeIndex === 0)) ? 'scale(0.8)' : 'scale(1)' }}
-                                >
-                                    <h1 className={styles.frame_content}>{frame}</h1>
-                                </div>
-                            );
-                        })}
+                  return (
+                      <div 
+                          key={index} 
+                          className={styles.frame} 
+                          style={{ transform: (Math.abs(index - activeIndex) >= 1) ? 'scale(0.8)' : 'scale(1)' }}
+                      >
+                          <h1 className={styles.frame_content}>{frame}</h1>
+                      </div>
+                  );
+              })}
             </div>
           </div>
           <button className={styles.arrow} onClick={addIndex}>
