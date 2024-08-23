7e8cdfae41989c1130f6349e0c51f3a0ce0ff445
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-08-22 18:10:41 -0700

    Created dark and lightmode feature

--- app/(pages)/day2/page.jsx
+++ app/(pages)/day2/page.jsx
@@ -1,37 +1,25 @@
 "use client";
 import styles from '../_styles/days/day2.module.scss';
-// import ImagesSlider from '../_components/day2/ImageSlider.tsx';
-// import ImagesSlider from '../_components/day2/ImageSlider.jsx';
 import Carousel from '../_components/day2/carousel.jsx';
 import React, { useState } from 'react';
-import Toggle from '../_components/day2/toggle';
-
-/* const handleChange = () => {
-    setIsDark(!isDark); // Toggle the state between true and false
-}; */
-
-
 
 export default function Day2(){
-    const [isDark, setIsDark] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleTheme = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
+    const themeStyles = {
+        backgroundColor: isDarkMode ? '#a9a9a9' : '#fff',
+        color: isDarkMode ? '#fff' : '#000'
+    };
 
     return(
-        <div className={styles.container} data-theme={isDark ? "dark" : "light"}>
-            <div className={styles.togglecontainer}>
-                <Toggle 
-                    isChecked={isDark} 
-                    handleChange={() => setIsDark(!isDark)} 
-                />
-
-                <input 
-                    className="input" 
-                    type="checkbox" 
-                    name="darkmode" 
-                    id="dark-mode"
-                    checked={isDark}
-                    onChange={() => setIsDark(!isDark)}  // Toggle dark mode on change
-                />
-            </div>
+        <div className={styles.container} data-theme={isDarkMode ? "dark" : "light"} style={themeStyles}>
+            <button onClick={toggleTheme}>
+                {isDarkMode ? 'Light' : 'Dark'} Mode
+            </button>
 
             <h1>Carousel</h1>
             <div className={styles.box}>
