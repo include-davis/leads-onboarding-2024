430225783d959b10cb470a7850eee7a9e0775853
tiramisuuuuuuu <ssmathew@ucdavis.edu>
2024-08-24 11:22:42 -0700

    Language dropdown context added

--- app/(pages)/_components/language_dropdown.jsx
+++ app/(pages)/_components/language_dropdown.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import styles from "@/app/(pages)/_styles/language_dropdown.module.scss";
+import { LangContext } from "../day2/lang";
 
 function Dropdown_Table(props) {
-    const lang_list = ["English", "Spanish", "Hindi"];
 
-    function setClose() {
+    function setClose(selectedLang) {
         props.setOpen(false);
+        props.setLang(selectedLang);
     }
 
-    const table = lang_list.map((lang)=>{return <button key={lang} onClick={()=>{setClose()}} className={styles.container}><h4 style={{color: "purple"}}>{lang}</h4></button>})
+    const table = props.lang_list.map((lang)=>{ 
+        return <button key={lang} onClick={()=>{setClose(lang)}} className={styles.container}>
+            <h4 style={{color: "purple"}}> {lang}</h4></button> 
+            })
     
     return (
         <div className={styles.table}>
@@ -20,20 +24,27 @@ function Dropdown_Table(props) {
 
 export default function Language_Dropdown() {
     const [open, setOpen] = useState(false);
+    const [lang, setLang] = useContext(LangContext);
+    const lang_list = {'en':"English", 'es':"Spanish", 'ml':"Malayalam"};
 
     function toggleOpen() {
         setOpen((prev)=>(!prev));
+    }
+
+    function get_currLang() {
+        
     }
 
     return (
         <div>
             <button className={styles.container} onClick={()=>{toggleOpen()}}>
                 <h4 style={{borderRight: "2px", borderLeft: "0", borderTop: "0", borderBottom: "0", 
-                    borderColor: "black", borderStyle: "solid", paddingRight: "5px", color: "purple"}}>Lng</h4>
-                <h4 style={{width: "70%", color: "purple"}}>English</h4>
+                    borderColor: "black", borderStyle: "solid", paddingRight: "5px", color: "purple"}}>
+                    Lng</h4>
+                <h4 style={{width: "70%", color: "purple"}}>{currLang}</h4>
                 {open ? <IoIosArrowDown style={{transform: "rotateX(180deg)"}}/> : <IoIosArrowDown />}
             </button>
-            {open && <Dropdown_Table setOpen={setOpen} />}
+            {open && <Dropdown_Table setOpen={setOpen} setLang={setLang} lang_list={lang_list}/>}
         </div>
     )
 }
--- app/(pages)/day2/page.jsx
+++ app/(pages)/day2/page.jsx
@@ -3,14 +3,19 @@
 import styles from "./page.module.scss";
 import Language_Dropdown from "@/app/(pages)/_components/language_dropdown";
 import Authentication_Box from "@/app/(pages)/_components/authentication_box";
+import { LangContext } from "./lang";
+import { useState } from "react";
 
 export default function Page() {
+    const [lang, setLang] = useState('en');
+
     return (
         <main className={styles.main}>
-            <div className={styles.header}>
-                <Language_Dropdown />
-            </div>
-            <Authentication_Box />
+            <LangContext.Provider value={ [lang, setLang] }>
+                <div className={styles.header}>
+                    <Language_Dropdown /></div>
+                <Authentication_Box />
+            </LangContext.Provider>
         </main>
     )
 }
